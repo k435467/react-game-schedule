b376c274eda68d290f3df5405d23dadd2ae4623a
--replace-all <k435467@gmail.com>
2021-10-23 09:57:02 +0800

    route test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -19,6 +22,32 @@ function App() {
           Learn React
         </a>
       </header>
+      <hr />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/profile/123">Profile 123</Link>
+          </li>
+          <li>
+            <Link to="/profile/456">Profile 456</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route path="/about" exact component={About} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/profile/:id" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
